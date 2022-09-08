71797094ca53894169a6283dbca511dc27e10c61
BenasB <29711974+BenasB@users.noreply.github.com>
2022-09-09 00:25:52 +0300

    Initial commit and example

---
+++ Apsitvarkom/Controllers/ExampleController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apsitvarkom.Api.Controllers
+{
+    [ApiController]
+    // Specifies that this controller endpoint (the uri to reach it) is '/api/Example'
+    [Route("/api/[controller]")]
+    public class ExampleController : ControllerBase
+    {
+        private readonly ILogger<ExampleController> _logger;
+
+        // Logger magically appears here in constructor
+        // This is due to it being registered in Dependecy Injection container (default behaviour of web api)
+        // This method of retrieving a dependency is called construction injection
+        public ExampleController(ILogger<ExampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        // This action does not specify a route, so it will be reachable at controller endpoint + '/' = '/api/Example'
+        [HttpGet]
+        // Additional documentation for swagger
+        [ProducesResponseType(typeof(ExampleModel), StatusCodes.Status200OK)]
+        public ExampleModel GetExample()
+        {
+            _logger.LogInformation("Example action called");
+
+            var result = new ExampleModel()
+            {
+                Text = "Hello from back end!"
+            };
+
+            // result automatically gets transformed to JSON and returned back to the requester
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Should be located in a separate file
+    /// Left here for the simplicity of example
+    /// </summary>
+    public class ExampleModel
+    {
+        public string Text { get; set; } = null!;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # apsitvarkom-be
+
 Apsitvarkom back end dotnet web API
+
+## Running
+
+1. Clone the repository
+2. Start project
+   1. Visual Studio
+      1. Open up solution
+      2. Press "Play" button at the top
+   2. [`dotnet` CLI](https://docs.microsoft.com/en-us/dotnet/core/tools/)
+      1. Open a terminal in `Apsitvarkom/Apsitvarkom.Api` directory
+      2. Run `dotnet run`
+3. Visit swagger at http://localhost:5125/swagger
+
+## References
+
+- [Web API tutorial](https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-6.0&tabs=visual-studio)
