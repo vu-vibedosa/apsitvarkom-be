6b9a2006ebc7960e06871f54cea3b5c08d28f19d
Šarūnas <sgriskus2002@gmail.com>
2022-09-15 16:49:16 +0300

    initial model commit

--- Apsitvarkom.Models/Location.cs
+++ Apsitvarkom.Models/Location.cs
@@ -1,56 +1,40 @@
-﻿namespace Apsitvarkom.Models
+﻿namespace Apsitvarkom.Models;
+
+/// <summary>
+/// Class for storing locations.
+/// </summary>
+public class Location
 {
+    private double _latitude;
+    private double _longitude;
+
     /// <summary>
-    /// Class for storing locations.
+    /// Latitude of the location. Cannot be less than -90.0 degrees or exceed 90.0 degrees.
     /// </summary>
-    public class Location
+    public double Latitude
     {
-        private double _latitude;
-        private double _longitude;
-
-        /// <summary>
-        /// Latitude of the location. Cannot be less than -90.0 degrees or exceed 90.0 degrees.
-        /// </summary>
-        public double Latitude 
-        { 
-            get
-            {
-                return _latitude;
-            } 
-            set
-            {
-                if (value >= -90.0 && value <= 90.0)
-                    _latitude = value;
-                else
-                    throw new ArgumentOutOfRangeException(nameof(Latitude), "Latitude cannot be less than -90.0 or exceed 90.0 degrees.");
-            }
-        }
-
-        /// <summary>
-        /// Longitude of the location. Cannot be less than -180 degrees or exceed 180 degrees.
-        /// </summary>
-        public double Longitude
+        get => _latitude;
+        set
         {
-            get
-            {
-                return _longitude;
-            }
-            set
-            {
-                if (value >= -180.0 && value <= 180.0)
-                    _longitude = value;
-                else
-                    throw new ArgumentOutOfRangeException(nameof(Longitude), "Longitude cannot be less than -180.0 or exceed 180.0 degrees.");
-            }
+            if (value >= -90.0 && value <= 90.0)
+                _latitude = value;
+            else
+                throw new ArgumentOutOfRangeException(nameof(Latitude), "Latitude cannot be less than -90.0 or exceed 90.0 degrees.");
         }
+    }
 
-        /// <summary>Constructor of location using latitude and longitude.</summary>
-        /// <param name="latitude">Latitude of the location.</param>
-        /// <param name="longitude">Longitude of the location./param>
-        public Location(double latitude, double longitude)
+    /// <summary>
+    /// Longitude of the location. Cannot be less than -180 degrees or exceed 180 degrees.
+    /// </summary>
+    public double Longitude
+    {
+        get => _longitude;
+        set
         {
-            Latitude = latitude;
-            Longitude = longitude;
+            if (value >= -180.0 && value <= 180.0)
+                _longitude = value;
+            else
+                throw new ArgumentOutOfRangeException(nameof(Longitude), "Longitude cannot be less than -180.0 or exceed 180.0 degrees.");
         }
     }
 }
--- Apsitvarkom.Models/PollutedLocation.cs
+++ Apsitvarkom.Models/PollutedLocation.cs
@@ -1,69 +1,41 @@
-﻿using System.Text.Json.Serialization;
-using static Apsitvarkom.Models.Enumerations;
+﻿using static Apsitvarkom.Models.Enumerations;
 
-namespace Apsitvarkom.Models
+namespace Apsitvarkom.Models;
+
+/// <summary>
+/// Class for storing captured polluted location records.
+/// </summary>
+public class PollutedLocation
 {
-    /// <summary>
-    /// Class for storing captured polluted location records.
-    /// </summary>
-    public class PollutedLocation
+    private int _progress;
+
+    /// <summary>Unique identifier of the given record.</summary>
+    public string Id { get; init; } = null!;
+
+    /// <summary>Geolocation of the given record.</summary>
+    public Location Location { get; set; } = null!;
+
+    /// <summary>Rough size estimate of the given record area in meters from the center.</summary>
+    public int Radius { get; set; }
+
+    /// <summary>Estimated current pollution level of the record.</summary>
+    public LocationSeverityLevel Severity { get; set; }
+
+    /// <summary><see cref="DateTime" /> of when the record was created.</summary>
+    public DateTime Spotted { get; init; }
+
+    /// <summary>Current progress of the record's cleaning process in percentages.</summary>
+    public int Progress
     {
-        /// <summary>Unique identifier of the given record.</summary>
-        public string Id { get; init; }
-
-        /// <summary>Location of the given record.</summary>
-        public Location Location { get; set; }
-
-        /// <summary>Rough size estimate of the given record in meters.</summary>
-        public int Radius { get; set; }
-
-        /// <summary>Estimated current pollution level of the record.</summary>
-        public LocationSeverityLevel Severity { get; set; }
-
-        /// <summary><see cref="DateTime"/> of when the record was created.</summary>
-        public DateTime Spotted { get; init; }
-
-        /// <summary>Current progress of the location's cleaning process in percentages.</summary>
-        public int Progress { get; set; }
-
-        /// <summary>Additional information about the record.</summary>
-        public string Notes { get; set; }
-
-        /// <summary>Constructor of a new polluted location record.</summary>
-        /// <param name="latitude">Location of the record.</param>
-        /// <param name="longitude">Location of the record.</param>
-        /// <param name="radius">Size of the recorded location.</param>
-        /// <param name="severity">Current levels of trash in the record site.</param>
-        /// <param name="notes">Additional information about the record.</param>
-        public PollutedLocation(double latitude, double longitude, int radius, int severity, string notes)
+        get => _progress;
+        set
         {
-            Id = Guid.NewGuid().ToString();
-            Location = new Location(latitude, longitude);
-            Radius = radius;
-            Severity = (LocationSeverityLevel)severity;
-            Progress = 0;
-            Spotted = DateTime.Now;
-            Notes = notes;
-        }
-
-        /// <summary>Constructor of an existing polluted location record.</summary>
-        /// <param name="id">Unique identifier of the record.</param>
-        /// <param name="location">Location of the record.</param>
-        /// <param name="radius">Size of the recorded location.</param>
-        /// <param name="severity">Current levels of trash in the record site.</param>
-        /// <param name="progress">Current progress of the cleaning process of the record site.</param>
-        /// <param name="spotted"><see cref="DateTime"/> when the location was recorded.</param>
-        /// <param name="notes">Additional information about the record.</param>
-        [JsonConstructor]
-        public PollutedLocation(string id, Location location, int radius, LocationSeverityLevel severity, int progress, DateTime spotted, string notes)
-        {
-            Id = id;
-            Location = location;
-            Radius = radius;
-            Severity = severity;
-            Progress = progress;
-            Spotted = spotted;
-            Notes = notes;
+            if (value >= 0 && value <= 100)
+                _progress = value;
+            throw new ArgumentOutOfRangeException(nameof(Progress), "Progress is depicted in percentages and can only have values between 0 and 100.");
         }
     }
+
+    /// <summary>Additional information about the record.</summary>
+    public string Notes { get; set; } = null!;
 }
