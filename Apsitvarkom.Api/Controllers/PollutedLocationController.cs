0ea24dfb485ccc2f399d256b90c75fcb962e72e5
XxDoMaSxX <dragunasdomas@gmail.com>
2022-09-21 23:10:45 +0300

    PollutedLocation controller refactored, formated.

--- Apsitvarkom.Api/Controllers/PollutedLocationController.cs
+++ Apsitvarkom.Api/Controllers/PollutedLocationController.cs
@@ -1,6 +1,6 @@
-﻿using Apsitvarkom.Models;
-using Apsitvarkom.Models.DTO;
+﻿using Apsitvarkom.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
+
 namespace Apsitvarkom.Api.Controllers;
 
 [ApiController]
@@ -14,8 +14,11 @@ public class PollutedLocationController : ControllerBase
         _logger = logger;
     }
 
-
-    private static IEnumerable<PollutedLocationDTO> pollutedLocations = new List<PollutedLocationDTO>
+    [HttpGet]
+    [Route("/[controller]/All")]
+    public ActionResult<IEnumerable<PollutedLocationDTO>> GetAll()
+    {
+        IEnumerable<PollutedLocationDTO> testData = new List<PollutedLocationDTO>()
         {
             new PollutedLocationDTO
             {
@@ -29,67 +32,24 @@ public class PollutedLocationController : ControllerBase
                 Severity = "MODERATE",
                 Spotted  =  DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ttt"),
                 Progress = 15,
-                Notes = "This is test notes"
-
+                Notes = "This is test notes Nr.1"
+            },
+            new PollutedLocationDTO
+            {
+                Id = "5be2354e-2500-4289-bbe2-66210592e17z",
+                Location = new LocationDTO
+                {
+                    Latitude = 15.1651,
+                    Longitude = 177.151,
+                },
+                Radius = 100,
+                Severity = "MODERATE",
+                Spotted  =  DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ttt"),
+                Progress = 55,
+                Notes = "This is test notes Nr.2"
             }
         };
 
-    [HttpGet("{id}")]
-    public ActionResult<PollutedLocationDTO> GetById(string id)
-    {
-        var pollutedLocation = pollutedLocations.FirstOrDefault(PollutedPlace => PollutedPlace.Id == id);
-        if (pollutedLocation == null)
-        {
-            return BadRequest("Polluted location is not found - wrong id provided or id is not provided at all");
-        }
-        return Ok(pollutedLocation);
-    }
-
-    [HttpGet]
-    [Route("/[controller]/All")]
-    public ActionResult<List<PollutedLocationDTO>> GetAll()
-    {
-
-        return Ok(pollutedLocations);
-    }
-
-    [HttpPost]
-    public ActionResult<IEnumerable<PollutedLocationDTO>> Insert(PollutedLocationDTO PolutedLocation)
-    {
-
-        var list = pollutedLocations.ToList();
-        list.Add(PolutedLocation);
-
-        pollutedLocations = list;
-        return Ok(PolutedLocation);
-    }
-
-    [HttpPut]
-    public ActionResult<List<PollutedLocation>> Update(PollutedLocationDTO PollutedLocation)
-    {
-
-        var list = pollutedLocations.ToList();
-        if (list.Remove(list.Where(predicate: x => x.Id == PollutedLocation.Id).FirstOrDefault()))
-        {
-            list.Add(PollutedLocation);
-            pollutedLocations = list;
-            return Ok(PollutedLocation);
-        }
-        return BadRequest();
-
-    }
-
-    [HttpDelete("{id}")]
-    public ActionResult<PollutedLocationDTO> Delete(string id)
-    {
-        var foundPollutionLocation = pollutedLocations.FirstOrDefault(pollutedPlace => pollutedPlace.Id == id);
-        if (foundPollutionLocation == null)
-        {
-            return BadRequest("Heroe not found");
-        }
-        var pollutedLocationList = pollutedLocations.ToList();
-        pollutedLocationList.Remove(foundPollutionLocation);
-        pollutedLocations = pollutedLocationList;
-        return Ok(pollutedLocations);
+        return Ok(testData);
     }
 }
