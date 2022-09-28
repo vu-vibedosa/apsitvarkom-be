ca2e2c355e996beb569508fc5ecce7dc72763f6e
Šarūnas <sgriskus2002@gmail.com>
2022-09-29 00:12:53 +0300

    code review changes

--- Apsitvarkom.UnitTests/Api/Controllers/PollutedLocationControllerTests.cs
+++ Apsitvarkom.UnitTests/Api/Controllers/PollutedLocationControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Apsitvarkom.Api.Controllers;
 using Apsitvarkom.DataAccess;
 using Apsitvarkom.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -8,8 +9,40 @@ namespace Apsitvarkom.UnitTests.Api.Controllers;
 
 public class PollutedLocationControllerTests
 {
-    private Mock<IPollutedLocationDTORepository> m_repository;
     private PollutedLocationController m_controller;
+    private Mock<IPollutedLocationDTORepository> m_repository;
+
+    private readonly IEnumerable<PollutedLocationDTO> PollutedLocationDTOs = new List<PollutedLocationDTO>
+    {
+        new()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Location = new LocationDTO
+            {
+                Latitude = 35.929673,
+                Longitude = -78.948237
+            },
+            Radius = 15,
+            Severity = "Moderate",
+            Spotted = "2022-09-14T17:35:23Z",
+            Progress = 42,
+            Notes = "Lorem ipsum"
+        },
+        new()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Location = new LocationDTO
+            {
+                Latitude = 111.11111,
+                Longitude = 11.11111
+            },
+            Radius = 11,
+            Severity = "Low",
+            Spotted = "2023-11-23T21:12:14Z",
+            Progress = 11,
+            Notes = "11111"
+        }
+    };
 
     [SetUp]
     public void SetUp()
@@ -24,44 +57,29 @@ public class PollutedLocationControllerTests
     [Test]
     public async Task GetAll_RepositoryReturnsPollutedLocationDTOs_OKActionResultReturned()
     {
-        var instances = GetPollutedLocationDTOs();
-        m_repository.Setup(self => self.GetAllAsync()).ReturnsAsync(instances);
+        m_repository.Setup(self => self.GetAllAsync()).ReturnsAsync(PollutedLocationDTOs);
 
         var actionResult = await m_controller.GetAll();
 
         var result = actionResult.Result as OkObjectResult;
 
         Assert.That(result, Is.Not.Null);
-        Assert.That(result!.StatusCode, Is.EqualTo(200));
-        Assert.That(result.Value, Is.EqualTo(instances));
+        Assert.That(result!.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
+        Assert.That(result.Value, Is.EqualTo(PollutedLocationDTOs));
     }
 
     [Test]
     public async Task GetById_RepositoryReturnsPollutedLocationDTO_OKActionResultReturned()
     {
-        var instanceId = Guid.NewGuid().ToString();
-        var instance = new PollutedLocationDTO
-        {
-            Id = instanceId,
-            Location = new LocationDTO
-            {
-                Latitude = 111.11111,
-                Longitude = 11.11111
-            },
-            Radius = 11,
-            Severity = "Low",
-            Spotted = "2023-11-23T21:12:14Z",
-            Progress = 11,
-            Notes = "11111"
-        };
-        m_repository.Setup(self => self.GetByIdAsync(instanceId)).ReturnsAsync(instance);
+        var instance = PollutedLocationDTOs.First();
+        m_repository.Setup(self => self.GetByIdAsync(instance.Id!)).ReturnsAsync(instance);
 
-        var actionResult = await m_controller.GetById(instanceId);
+        var actionResult = await m_controller.GetById(instance.Id!);
 
         var result = actionResult.Result as OkObjectResult;
 
         Assert.That(result, Is.Not.Null);
-        Assert.That(result!.StatusCode, Is.EqualTo(200));
+        Assert.That(result!.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
         Assert.That(result.Value, Is.EqualTo(instance));
     }
 
@@ -76,42 +94,6 @@ public class PollutedLocationControllerTests
         var result = actionResult.Result as NotFoundObjectResult;
 
         Assert.That(result, Is.Not.Null);
-        Assert.That(result!.StatusCode, Is.EqualTo(404));
-    }
-
-    private IEnumerable<PollutedLocationDTO> GetPollutedLocationDTOs()
-    {
-        var instances = new List<PollutedLocationDTO>
-        {
-            new()
-            {
-                Id = Guid.NewGuid().ToString(),
-                Location = new LocationDTO
-                {
-                    Latitude = 35.929673,
-                    Longitude = -78.948237
-                },
-                Radius = 15,
-                Severity = "Moderate",
-                Spotted = "2022-09-14T17:35:23Z",
-                Progress = 42,
-                Notes = "Lorem ipsum"
-            },
-            new()
-            {
-                Id = Guid.NewGuid().ToString(),
-                Location = new LocationDTO
-                {
-                    Latitude = 111.11111,
-                    Longitude = 11.11111
-                },
-                Radius = 11,
-                Severity = "Low",
-                Spotted = "2023-11-23T21:12:14Z",
-                Progress = 11,
-                Notes = "11111"
-            }
-        };
-        return instances;
+        Assert.That(result!.StatusCode, Is.EqualTo(StatusCodes.Status404NotFound));
     }
 }
