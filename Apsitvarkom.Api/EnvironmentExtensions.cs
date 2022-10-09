f300e6728b0a1576b919af82a3571223cc2adea2
BenasB <29711974+BenasB@users.noreply.github.com>
2022-10-09 22:31:57 +0300

    feat(apsitvarkom-fe#19): enable cors to allow calls from front end based on environment

---
+++ Apsitvarkom.Api/EnvironmentExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Apsitvarkom.Api
+{
+    internal static class EnvironmentExtensions
+    {
+        internal static bool IsLocal(this IWebHostEnvironment hostingEnvironment) => hostingEnvironment.IsEnvironment("Local");
+    }
+}
