18870b87602f9e01453af9bb9448bc174a9b838f
Šarūnas <sgriskus2002@gmail.com>
2022-10-31 21:43:43 +0200

    tests written to cover google geocoder implementation

--- Apsitvarkom.UnitTests/DataAccess/GoogleGeocoderTests.cs
+++ Apsitvarkom.UnitTests/DataAccess/GoogleGeocoderTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Apsitvarkom.Configuration;
 using Apsitvarkom.DataAccess;
+using Apsitvarkom.Models;
 using Moq;
 using Moq.Protected;
 
@@ -8,41 +9,81 @@ namespace Apsitvarkom.UnitTests.DataAccess;
 
 public class GoogleGeocoderTests
 {
-    private static readonly IApiKeyProvider _apiKeyProvider = new Mock<IApiKeyProvider>().Object;
+    private Mock<IApiKeyProvider> _apiKeyProvider;
+    private Mock<HttpMessageHandler> _handlerMock;
+    private IGeocoder _geocoder;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _apiKeyProvider = new Mock<IApiKeyProvider>();
+        _handlerMock = new Mock<HttpMessageHandler>();
+        var httpClient = new HttpClient(_handlerMock.Object)
+        {
+            BaseAddress = new Uri("https://mockreversegeocoder.com")
+        };
+        _geocoder = new GoogleGeocoder(httpClient, _apiKeyProvider.Object);
+    }
 
     [Test]
-    public async Task SuccessfullyFindsFirstAddress()
+    public void OkStatusCodeReturned_SingleAddressRetrieved_TitleSuccessfullyReturned()
     {
         const string title = "Hello World";
-        const string responseJsonString = "{\"results\":[{\"formatted_address\":\"" + title + "\"}, {\"formatted_address\": \"second address\"}]}";
+        const string responseJsonString = "{\"results\":[{\"formatted_address\":\"" + title + "\"}], \"status\": \"OK\"}";
 
-        var handlerMock = new Mock<HttpMessageHandler>();
+        TestReverseGeocodeRequest(responseJsonString, title);
+    }
+
+    [Test]
+    public void OkStatusCodeReturned_SeveralAddressesRetrieved_FirstTitleReturned()
+    {
+        const string title = "Hello World";
+        const string responseJsonString = "{\"results\":[{\"formatted_address\":\"" + title + "\"}, {\"formatted_address\": \"second address\"}], \"status\": \"OK\"}";
+
+        TestReverseGeocodeRequest(responseJsonString, title);
+    }
+
+    [Test]
+    public void OkStatusCodeReturned_NullAddressRetrieved_NullReturned()
+    {
+        const string responseJsonString = "{\"results\":[{\"formatted_address\":null}], \"status\": \"OK\"}";
+
+        TestReverseGeocodeRequest(responseJsonString);
+    }
+
+    [Test]
+    public void ZeroResultsStatusCodeRetrieved_NullReturned() =>
+        TestReverseGeocodeRequest("{\"results\":[], \"status\": \"ZERO_RESULTS\"}");
+
+    [Test]
+    public void EmptyResultsResponseRetrieved_NullReturned() =>
+        TestReverseGeocodeRequest("{\"results\":null}");
+
+    [Test]
+    [TestCase("")]
+    [TestCase("null")]
+    [TestCase("{}")]
+    public void EmptyResponseRetrieved_NullReturned(string response) =>
+        TestReverseGeocodeRequest(response);
+
+    private async void TestReverseGeocodeRequest(string responseJsonString, string? expectedResult = null)
+    {
         var response = new HttpResponseMessage
         {
             StatusCode = HttpStatusCode.OK,
             Content = new StringContent(responseJsonString),
         };
 
-        handlerMock
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
+        _handlerMock.Protected().Setup<Task<HttpResponseMessage>>(
                 "SendAsync",
                 ItExpr.IsAny<HttpRequestMessage>(),
                 ItExpr.IsAny<CancellationToken>())
             .ReturnsAsync(response);
 
-        var httpClient = new HttpClient(handlerMock.Object);
-        httpClient.BaseAddress = new Uri("https://anything.com");
-        IGeocoder geocoder = new GoogleGeocoder(httpClient, _apiKeyProvider);
+        var result = await _geocoder.ReverseGeocodeAsync(new Coordinates());
 
-        var result = await geocoder.ReverseGeocodeAsync(new()
-        {
-            Latitude = 0,
-            Longitude = 0
-        });
-
-        Assert.That(result, Is.EqualTo(title));
-        handlerMock.Protected().Verify(
+        Assert.That(result, Is.EqualTo(expectedResult));
+        _handlerMock.Protected().Verify(
             "SendAsync",
             Times.Exactly(1),
             ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
