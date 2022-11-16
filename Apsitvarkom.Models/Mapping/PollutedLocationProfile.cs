18931c6854d534d5b74d089eaed0e764bec53c31
BenasB <29711974+BenasB@users.noreply.github.com>
2022-11-17 01:59:41 +0200

    Simplified creation model

--- Apsitvarkom.Models/Mapping/PollutedLocationProfile.cs
+++ Apsitvarkom.Models/Mapping/PollutedLocationProfile.cs
@@ -19,10 +19,12 @@ public class PollutedLocationProfile : Profile
     private void MapRequests()
     {
         CreateMap<CoordinatesCreateRequest, Coordinates>();
-        CreateMap<LocationCreateRequest, Location>();
+        CreateMap<CoordinatesCreateRequest, Location>()
+            .ForMember(dest => dest.Coordinates, opt => opt.MapFrom(source => source));
         CreateMap<PollutedLocationCreateRequest, PollutedLocation>()
             .ForMember(dest => dest.Spotted, opt => opt.MapFrom(_ => DateTime.UtcNow))
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()));
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()))
+            .ForMember(dest => dest.Location, opt => opt.MapFrom(source => source.Coordinates));
     }
 
     private void MapResponses()
