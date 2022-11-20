92dcea67b361c20df6d5d51abfca377fafe5cff4
Šarūnas <sgriskus2002@gmail.com>
2022-11-21 00:03:24 +0200

    identify request and its validator created (mapping profile not created intentionally)

---
+++ Apsitvarkom.Models/Public/PollutedLocationIdentifyRequest.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Apsitvarkom.Models.Public;
+
+public class PollutedLocationIdentifyRequest
+{
+    public Guid? Id { get; set; }
+}
+
+public class PollutedLocationIdentifyRequestValidator : AbstractValidator<PollutedLocationIdentifyRequest>
+{
+    public PollutedLocationIdentifyRequestValidator()
+    {
+        RuleFor(l => l.Id).NotNull();
+    }
+}
