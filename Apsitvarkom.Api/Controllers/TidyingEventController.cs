9475e23db36c79391c3fa83f569405d79a0a3a3b
Šarūnas <sgriskus2002@gmail.com>
2022-11-25 22:32:01 +0200

    introduced a controller

---
+++ Apsitvarkom.Api/Controllers/TidyingEventController.cs
@@ -0,0 +1,20 @@
+﻿using Apsitvarkom.DataAccess;
+using Apsitvarkom.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apsitvarkom.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class TidyingEventController : ControllerBase
+{
+    private readonly IRepository<TidyingEvent> _repository;
+    private readonly IMapper _mapper;
+
+    public TidyingEventController(IRepository<TidyingEvent> repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+}
