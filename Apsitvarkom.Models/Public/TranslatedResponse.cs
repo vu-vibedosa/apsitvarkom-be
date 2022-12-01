b88679ea4b96afa788357ad16181e8aee96e22cb
Šarūnas <sgriskus2002@gmail.com>
2022-12-02 00:22:05 +0200

    minor adjustment

--- Apsitvarkom.Models/Public/TranslatedResponse.cs
+++ Apsitvarkom.Models/Public/TranslatedResponse.cs
@@ -4,8 +4,8 @@ namespace Apsitvarkom.Models.Public;
 
 public class TranslatedResponse<T>
 {
-    public T? en { get; set; }
-    public T? lt { get; set; }
+    public T en { get; set; }
+    public T lt { get; set; }
 }
 
 public class TranslatedResponseValidator<T> : AbstractValidator<TranslatedResponse<T>>
