6f49b4482aa3aafa016749a6e242fec43f71b11b
Šarūnas <sgriskus2002@gmail.com>
2022-12-11 19:13:03 +0200

    changed middleware to an example which brings more benefit

---
+++ Apsitvarkom.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Apsitvarkom.Api.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IWebHostEnvironment _environment;
+        private readonly JsonSerializerOptions _options;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, IWebHostEnvironment environment)
+        {
+            _next = next;
+            _environment = environment;
+            _options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var errorMessage = "An internal server error has occurred. If this error persists, please contact product's owners.";
+
+            if (_environment.IsDevelopment())
+            {
+                errorMessage = JsonSerializer.Serialize(exception, _options);
+            }
+
+            return context.Response.WriteAsync(errorMessage);
+        }
+    }
+
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
